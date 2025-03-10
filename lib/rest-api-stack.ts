9f646b790637f4e8cc305c07aab9e4959257cb83
healt-tracker-rest <fardin1281@gmail.com>
2025-03-10 21:34:11 +0000

    API resource to query cast members

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -8,7 +8,7 @@ import * as apig from "aws-cdk-lib/aws-apigateway";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { generateBatch } from "../shared/util";
-import { movies } from "../seed/movies";
+import { movies, movieCasts } from "../seed/movies";
 
 export class RestAPIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,6 +21,19 @@ export class RestAPIStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       tableName: "Movies",
     });
+    const movieCastsTable = new dynamodb.Table(this, "MovieCastTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "movieId", type: dynamodb.AttributeType.NUMBER },
+      sortKey: { name: "actorName", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieCast",
+    });
+
+    movieCastsTable.addLocalSecondaryIndex({
+      indexName: "roleIx",
+      sortKey: { name: "roleName", type: dynamodb.AttributeType.STRING },
+    });
+
 
     
     // Functions 
@@ -82,6 +95,24 @@ export class RestAPIStack extends cdk.Stack {
             REGION: "eu-west-1",
           },
         });
+
+        // Movie cast function
+    const getMovieCastMembersFn = new lambdanode.NodejsFunction(
+      this,
+      "GetCastMemberFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_22_X,
+        entry: `${__dirname}/../lambdas/getMovieCastMember.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieCastsTable.tableName,
+          REGION: "eu-west-1",
+        },
+      }
+    );
+
         
 
         
@@ -92,20 +123,24 @@ export class RestAPIStack extends cdk.Stack {
             parameters: {
               RequestItems: {
                 [moviesTable.tableName]: generateBatch(movies),
+                [movieCastsTable.tableName]: generateBatch(movieCasts),  // Added
               },
             },
             physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
           },
           policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
-            resources: [moviesTable.tableArn],
+            resources: [moviesTable.tableArn, movieCastsTable.tableArn],  // Includes movie cast
           }),
         });
+    
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
         moviesTable.grantReadWriteData(newMovieFn)
         moviesTable.grantReadWriteData(deleteMovieFn);
+        movieCastsTable.grantReadData(getMovieCastMembersFn);
+
 
 
 
@@ -146,6 +181,14 @@ export class RestAPIStack extends cdk.Stack {
       "DELETE",
       new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
     );
+    const movieCastEndpoint = moviesEndpoint.addResource("cast");
+
+    // Movie cast endpoint
+    movieCastEndpoint.addMethod(
+        "GET",
+        new apig.LambdaIntegration(getMovieCastMembersFn, { proxy: true })
+    );
+
         
       }
     }
--- seed/movies.ts
+++ seed/movies.ts
@@ -1,4 +1,5 @@
-import {Movie} from '../shared/types'
+import {Movie, MovieCast} from '../shared/types'
+
 
 export const movies : Movie[] = [
   {
@@ -320,7 +321,36 @@ export const movies : Movie[] = [
     video: false,
     vote_average: 7.4,
     vote_count: 325
-  }
-]
+  },
+];
+
+  export const movieCasts: MovieCast[] = [
+    {
+      movieId: 1234,
+      actorName: "Joe Bloggs",
+      roleName: "Male Character 1",
+      roleDescription: "description of character 1",
+    },
+    {
+      movieId: 1234,
+      actorName: "Alice Broggs",
+      roleName: "Female Character 1",
+      roleDescription: "description of character 2",
+    },
+    {
+      movieId: 1234,
+      actorName: "Joe Cloggs",
+      roleName: "Male Character 2",
+      roleDescription: "description of character 3",
+    },
+    {
+      movieId: 2345,
+      actorName: "Joe Bloggs",
+      roleName: "Male Character 1",
+      roleDescription: "description of character 3",
+    },
+  ];
+  
+
 
 
